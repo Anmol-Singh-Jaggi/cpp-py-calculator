c6811d89acda1dd84d7fb507848d3d913da411df
Anmol <anmolj.65@gmail.com>
2015-02-18 06:15:46 +0530

    Maintenance
    - New indentation style.
    - Conformance to C++ 11.
    - Conformance to a new set of warnings.

--- cpp-py-calc.cpp
+++ cpp-py-calc.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    cout<<"Enter the expression to evaluate\n";
-    string x;
-    cin>>x;
-    system((string("python -c \"print ")+x+string("\"")).c_str());
+	cout << "Enter the expression to evaluate\n";
+	string x;
+	getline( cin, x );
+	string command = "python -c \"print(" + x + "\")";  // Assuming Python 3
+	system( command.c_str() );
 }
